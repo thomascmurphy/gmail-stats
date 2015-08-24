da1fd8803cc65812df0b9458b4544319728bb0b5
Thomas Murphy <thomas.c.murphy@gmail.com>
2015-08-24 17:26:03 -0600

    default last_update

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,4 +1,10 @@
 class UserGroup < ActiveRecord::Base
   has_many :users
   has_many :admins
+
+  before_create :set_last_update
+  def set_last_update
+    self.last_update = Time.now
+  end
+
 end
